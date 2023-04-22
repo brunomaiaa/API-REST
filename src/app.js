243004976f256ff7b11409dab4246eaf92da13ec
Bruno Maia <111490358+brunomaiaa@users.noreply.github.com>
2023-04-22 13:34:39 +0200

    creation of the functions search selection by id and search index by id
    creation of the functions search selection by id and search index by id

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ const selecoes = [
     {id: 4, nome: 'França', grupo: 'G'},
 ]
 
+function buscarSelecaoPorId(id) {
+    return selecoes.filter( selecao => selecao.id == id)
+}
+
+function buscarIndexSelecao (id) {
+    return selecoes.findIndex( selecao => selecao.id == id)
+}
+
 
 // create default route
 app.get('/', (req, res) => {
@@ -23,11 +31,27 @@ app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
     })
 
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
     app.post('/selecoes', (req, res) => {
         selecoes.push(req.body)
         res.status(201).send('Successfully registered selection')
-
-
     })
+
+    app.delete('/selecoes/:id', (req, res) => {
+        let index = buscarSelecaoPorId(req.params.id)
+        selecoes.splice(index, 1)
+        res.status(200).send('Successfully deleted selection' )
+    })
+
+    app.put('/selecoes/:id', (req, res) => {
+        let index = buscarSelecaoPorId(req.params.id)
+        selecoes[index].selecao = req.body.selecao
+        selecoes[index].grupo   = req.body.grupo
+        res.status(200).json(selecoes)
+    })
+
 
 export default app
